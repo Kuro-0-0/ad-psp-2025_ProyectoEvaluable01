122f06482c8af64ceda0b3dae3c239ef514a3ca7
Pablo Garcia - Clase <garcia.mapab24@triana.salesianos.edu>
2025-11-25 11:47:47 +0100

    Spring initializer

---
+++ src/main/java/com/salesianostriana/dam/proyecto_evaluable/ProyectoEvaluable01Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyecto_evaluable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEvaluable01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEvaluable01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/proyecto_evaluable/ProyectoEvaluable01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyecto_evaluable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEvaluable01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
