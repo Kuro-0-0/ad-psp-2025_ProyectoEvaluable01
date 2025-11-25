9ae98a953d5323a534c0142d80c144a3c35a7a7d
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2025-11-25 19:09:03 +0100

    Exceptions ready

---
+++ src/main/java/com/salesianostriana/dam/proyecto_evaluable/errors/exceptions/DuplicatedNameException.java
@@ -0,0 +1,7 @@
+package com.salesianostriana.dam.proyecto_evaluable.errors.exceptions;
+
+public class DuplicatedNameException extends RuntimeException {
+    public DuplicatedNameException(String message) {
+        super(message);
+    }
+}
