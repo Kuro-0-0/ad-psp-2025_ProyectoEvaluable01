93487f10cc01504253cc7014746d7250cd30502d
Pablo Garcia - Clase <garcia.mapab24@triana.salesianos.edu>
2025-11-27 09:59:39 +0100

    CategoriaController Finalizado, funcionalidad y documentacion.

--- src/main/java/com/salesianostriana/dam/proyecto_evaluable/controllers/CategoriaController.java
+++ src/main/java/com/salesianostriana/dam/proyecto_evaluable/controllers/CategoriaController.java
@@ -4,20 +4,19 @@ import com.salesianostriana.dam.proyecto_evaluable.models.dtos.categoria.Categor
 import com.salesianostriana.dam.proyecto_evaluable.models.dtos.categoria.CategoriaResponseDTO;
 import com.salesianostriana.dam.proyecto_evaluable.services.CategoriaService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -115,7 +114,7 @@ public class CategoriaController {
             )
             @RequestBody CategoriaRequestDTO categoriaDTO
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(categoriaDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(CategoriaResponseDTO.toDTO(service.create(categoriaDTO.fromDTO())));
     }
 
     @GetMapping()
@@ -163,19 +162,183 @@ public class CategoriaController {
             )
     )
     public ResponseEntity<List<CategoriaResponseDTO>> listarCategorias() {
-        return ResponseEntity.ok(service.list());
+        return ResponseEntity.ok(service.list().stream().map(CategoriaResponseDTO::toDTO).toList());
     }
 
-    public ResponseEntity<CategoriaResponseDTO> verCategoria() {
-        return null;
+    @GetMapping("/{id}")
+    @Operation(summary = "Ver categoría", description = "Obtiene los detalles de una categoría específica por su ID.")
+    @ApiResponse(
+            responseCode = "404",
+            description = "Categoría no encontrada",
+            content = @Content(
+                    mediaType = "application/JSON",
+                    schema = @Schema(implementation = ProblemDetail.class),
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            {
+                                              "type": "about:blank",
+                                              "title": "La entidad solicitada no se encuentra en la BBDD.",
+                                              "status": 404,
+                                              "detail": "No se ha encontrado la categoria con id: 1",
+                                              "instance": "/api/v1/categorias/1"
+                                            }
+                                            """
+                            )
+                    }
+            )
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Categoría obtenida correctamente.",
+            content = @Content(
+                    mediaType = "application/JSON",
+                    schema = @Schema(implementation = CategoriaResponseDTO.class),
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            {
+                                              "id": 1,
+                                              "nombre": "Postres",
+                                              "descripcion": "Categoría para recetas de postres y dulces."
+                                            }
+                                            """
+                            )
+                    }
+            )
+    )
+    public ResponseEntity<CategoriaResponseDTO> verCategoria(
+            @Parameter(required = true, example = "1", description = "ID de la categoría que se desea obtener.")
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(CategoriaResponseDTO.toDTO(service.read(id)));
     }
 
-    public ResponseEntity<CategoriaResponseDTO> editarCategoria(CategoriaRequestDTO categoriaDTO) {
-        return null;
+    @PutMapping("/{id}")
+    @Operation(summary = "Editar categoría", description = "Permite editar una categoría existente proporcionando su ID y los nuevos datos.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Categoría editada correctamente.",
+            content = @Content(
+                    mediaType = "application/JSON",
+                    schema = @Schema(implementation = CategoriaResponseDTO.class),
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            {
+                                              "id": 1,
+                                              "nombre": "Postres Actualizados",
+                                              "descripcion": "Categoría actualizada para recetas de postres y dulces."
+                                            }
+                                            """
+                            )
+                    }
+            )
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Categoría no encontrada",
+            content = @Content(
+                    mediaType = "application/JSON",
+                    schema = @Schema(implementation = ProblemDetail.class),
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            {
+                                              "type": "about:blank",
+                                              "title": "La entidad solicitada no se encuentra en la BBDD.",
+                                              "status": 404,
+                                              "detail": "No se ha encontrado la categoria con id: 1",
+                                              "instance": "/api/v1/categorias/1"
+                                            }
+                                            """
+                            )
+                    }
+            )
+    )
+    @ApiResponse(
+            responseCode = "409",
+            description = "Solicitud inválida. Los datos proporcionados no son correctos.",
+            content = @Content(
+                    mediaType = "application/JSON",
+                    schema = @Schema(implementation = ProblemDetail.class),
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            {
+                                              "type": "about:blank",
+                                              "title": "El nombre insertado ya se encuentra en la BBDD.",
+                                              "status": 409,
+                                              "detail": "Ya existe una categoría con el nombre: Frescos",
+                                              "instance": "/api/v1/categorias/1"
+                                            }
+                                            """
+                            )
+                    }
+            )
+    )
+    public ResponseEntity<CategoriaResponseDTO> editarCategoria(
+            @Parameter(required = true, description = "ID de la categoría que se desea editar.", example = "1")
+            @PathVariable  Long id,
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    description = "Datos actualizados de la categoría.",
+                    content = @Content(
+                            mediaType = "application/JSON",
+                            schema = @Schema(implementation = CategoriaRequestDTO.class),
+                            examples = {
+                                    @ExampleObject(
+                                            value = """
+                                                    {
+                                                      "nombre": "Postres Actualizados",
+                                                      "descripcion": "Categoría actualizada para recetas de postres y dulces."
+                                                    }
+                                                    """
+                                    )
+                            }
+                    )
+            )
+            @RequestBody CategoriaRequestDTO categoria) {
+        return ResponseEntity.ok().body(CategoriaResponseDTO.toDTO(service.update(id, categoria.fromDTO())));
     }
 
-    public ResponseEntity<String> eliminarCategoria() {
-        return null;
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar categoría", description = "Elimina una categoría específica por su ID.")
+    @ApiResponse(
+            responseCode = "204",
+            description = "Categoría eliminada correctamente."
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Categoría no encontrada",
+            content = @Content(
+                    mediaType = "application/JSON",
+                    schema = @Schema(implementation = ProblemDetail.class),
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            {
+                                              "type": "about:blank",
+                                              "title": "La entidad solicitada no se encuentra en la BBDD.",
+                                              "status": 404,
+                                              "detail": "No se ha encontrado la categoria con id: 1",
+                                              "instance": "/api/v1/categorias/1"
+                                            }
+                                            """
+                            )
+                    }
+            )
+    )
+    public ResponseEntity<String> eliminarCategoria(
+            @Parameter(
+                    required = true,
+                    description = "ID de la categoría que se desea eliminar.",
+                    example = "1"
+            )
+            @PathVariable  Long id
+            ) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
