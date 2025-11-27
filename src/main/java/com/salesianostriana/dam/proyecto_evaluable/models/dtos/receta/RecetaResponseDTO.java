4ae82e182d2fcbea4934aecaccc5d6a4cd240c77
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2025-11-27 19:43:51 +0100

    RecetaResponseDTO actualizado...

--- src/main/java/com/salesianostriana/dam/proyecto_evaluable/models/dtos/receta/RecetaResponseDTO.java
+++ src/main/java/com/salesianostriana/dam/proyecto_evaluable/models/dtos/receta/RecetaResponseDTO.java
@@ -1,10 +1,14 @@
 package com.salesianostriana.dam.proyecto_evaluable.models.dtos.receta;
 
 import com.salesianostriana.dam.proyecto_evaluable.models.Receta;
+import com.salesianostriana.dam.proyecto_evaluable.models.dtos.recetaIngrediente.RecetaIngredienteResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Builder
 @Data
@@ -16,15 +20,20 @@ public class RecetaResponseDTO {
     private String nombre;
     private Integer tiempoPreparacionMin;
     private String dificultad;
+    private Set<RecetaIngredienteResponseDTO> ingredientes;
     private String categoria;
 
     public static RecetaResponseDTO toDTO(Receta r) {
+
+        Set<RecetaIngredienteResponseDTO> ingredientes = r.getIngredientes().stream().map(RecetaIngredienteResponseDTO::toDTO).collect(Collectors.toSet());
+
         return RecetaResponseDTO.builder()
                 .id(r.getId())
                 .nombre(r.getNombre())
                 .tiempoPreparacionMin(r.getTiempoPreparacionMin())
                 .dificultad(r.getDificultad().name())
                 .categoria(r.getCategoria().getNombre())
+                .ingredientes(ingredientes)
                 .build();
     }
 
