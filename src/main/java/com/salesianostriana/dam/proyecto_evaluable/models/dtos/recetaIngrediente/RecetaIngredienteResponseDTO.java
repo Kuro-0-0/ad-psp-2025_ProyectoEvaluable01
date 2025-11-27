d562e8bc2ee237307d935ffe141d52f1040ac8da
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2025-11-27 19:14:23 +0100

    RecetaIngredienteResponseDTO Listo...

---
+++ src/main/java/com/salesianostriana/dam/proyecto_evaluable/models/dtos/recetaIngrediente/RecetaIngredienteResponseDTO.java
@@ -0,0 +1,30 @@
+package com.salesianostriana.dam.proyecto_evaluable.models.dtos.recetaIngrediente;
+
+import com.salesianostriana.dam.proyecto_evaluable.models.RecetaIngrediente;
+import com.salesianostriana.dam.proyecto_evaluable.models.dtos.ingrediente.extras.RecetasIngredienteResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RecetaIngredienteResponseDTO {
+
+    private String nombreIngrediente;
+    private Double cantidad;
+    private String unidad;
+
+    private RecetaIngredienteResponseDTO toDTO(RecetaIngrediente recetaIngrediente){
+        return RecetaIngredienteResponseDTO.builder()
+                .nombreIngrediente(recetaIngrediente.getIngrediente().getNombre())
+                .cantidad(recetaIngrediente.getCantidad())
+                .unidad(recetaIngrediente.getUnidad())
+                .build();
+
+
+    }
+
+}
